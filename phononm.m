3b96a790294123fb81de7c7365bde264a189b3e7
Camilo Chaparro <33160873+Camilokastel160@users.noreply.github.com>
2018-04-12 04:28:36 -0500

    Update phononm.m

--- phononm.m
+++ phononm.m
@@ -5,6 +5,6 @@ K=3         %Spectral density (On construction)
 dy = zeros (3,1);
 dy(1)=-((Omega/(2*tau*sqrt(pi)))*exp(-(t./tau)^2))*y(3);    %p11 
 dy(2)=-K*y(2);                                              %Re(p10)  
-dy(3)=((Omega/(2*tau*sqrt(pi)))*exp(-(t./tau)^2))*(1/2)*(1-2*y(1))+K*y(3);   %Re(p11)
+dy(3)=((Omega/(2*tau*sqrt(pi)))*exp(-(t./tau)^2))*(1/2)*(1-2*y(1))+K*y(3);   %Re(p10)
 end
 
