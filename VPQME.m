ac8232e8f4d6751eec146b898c5d4c79544d2de7
Camilo Chaparro <33160873+Camilokastel160@users.noreply.github.com>
2018-04-14 01:17:48 -0500

    Update VPQME.m
    -Matrix form of differential equations
    -Rename of the variables properly
    -Inclusion of the spectral density function

--- VPQME.m
+++ VPQME.m
@@ -1,9 +1,19 @@
-function drho = VPQME(t,rho)
-Omega=1;                                                   % Pulse area      
-tau=14;                                                    % Full width half maximum
-K=3;                                                       % Spectral density (On construction)
-drho(1)=1-((Omega/(2*tau*sqrt(pi)))*exp(-(t./tau)^2))*rho(3);                    % p00 
-drho(2)=-K*rho(2);                                                               % Re(p10)  
-drho(3)=((Omega/(2*tau*sqrt(pi)))*exp(-(t./tau)^2))*(1/2)*(1-2*rho(1))+K*rho(3); % Re(p10)
-drho(4)=1-drho(1);
+%Preliminar version of VPQME
+
+function drho = phononm(t,rho)
+Theta=30                                                   
+tau=14e-12                                            %Full width half maximum in s
+hbar=1.0545718e-34                                    %Planck constant  J.s
+T=25                                                  %Temperature in K
+kb=1.38064852e-23                                     %Boltzmann constant J/K
+wc=2.2e12                                             %1/s
+A=pi*0.027*1e-24                                      %s^2
+Omega=(Theta/(2*tau*sqrt(pi)))*exp(-(t./(2*tau))^2)    %1/s
+K=coth(hbar*Omega/(2*kb*T))*A*((Omega)^3)*exp(-(Omega/wc)^2)*pi/2   %Spectral density                 
+
+
+drho = zeros (3,1);        
+
+drho=[0 0 -Omega;0 -K 0; Omega 0 -K]*rho+[0;0;-Omega/2]
+
 end
