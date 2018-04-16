2c27926cc3a2a8a1835964feec1b5232770019cf
Camilo Chaparro <33160873+Camilokastel160@users.noreply.github.com>
2018-04-16 17:07:18 -0500

    Updates of sampleInput_VPQME.m and VPQME.m
    Preliminar versions of the mentioned files.

--- VPQME.m
+++ VPQME.m
@@ -1,9 +1,22 @@
 function drho = VPQME(t,rho)
-Omega=1;                                                   % Pulse area      
-tau=14;                                                    % Full width half maximum
-K=3;                                                       % Spectral density (On construction)
-drho(1)=1-((Omega/(2*tau*sqrt(pi)))*exp(-(t./tau)^2))*rho(3);                    % p00 
-drho(2)=-K*rho(2);                                                               % Re(p10)  
-drho(3)=((Omega/(2*tau*sqrt(pi)))*exp(-(t./tau)^2))*(1/2)*(1-2*rho(1))+K*rho(3); % Re(p10)
-drho(4)=1-drho(1);
+
+%% Fundamental constants
+kb=1.38064852e-23;                                     %Boltzmann constant J/K
+hbar=1.0545718e-34;                                    %Planck constant  J.s
+
+%% System parameters
+
+Theta=30;                                                   
+tau=14e-12;                                            %Full width half maximum in s
+T=25;                                                  %Temperature in K
+wc=2.2e12;                                             %1/s
+A=pi*0.027*1e-24; 
+
+Omega=(Theta/(2*tau*sqrt(pi)))*exp(-(t./(2*tau))^2);    %Rabi frequency laser in 1/s
+K=coth(hbar*Omega/(2*kb*T))*A*((Omega)^3)*exp(-(Omega/wc)^2)*pi/2;   %Spectral density combined with temperature dependent term                 
+
+%% Master equation
+
+drho = zeros (3,1);        
+drho=[0 0 -Omega;0 -K 0; Omega 0 -K]*rho+[0;0;-Omega/2];     %Systems of differential equations.
 end
