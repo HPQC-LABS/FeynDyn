0c5a4e3b8730c000b2e9d807249a30a65051050c
Camilo Chaparro <33160873+Camilokastel160@users.noreply.github.com>
2018-04-20 15:23:49 -0500

    Update VPQME using the function Interpolate.
    The function doesn't contains parameters, constants and variables of the system.

--- VPQME.m
+++ VPQME.m
@@ -1,22 +1,10 @@
-function drho = VPQME(t,rho)
+function drho = VPQME(t,rho,Kt,K,Ot,Omega,kb,hbar,Theta,tau,T,wc,alpha)
+%% Interpolate functions
 
-%% Fundamental constants
-kb=1.38064852e-23;                                     %Boltzmann constant J/K
-hbar=1.0545718e-34;                                    %Planck constant  J.s
-
-%% System parameters
-
-Theta=30;                                                   
-tau=14e-12;                                            %Full width half maximum in s
-T=25;                                                  %Temperature in K
-wc=2.2e12;                                             %1/s
-A=pi*0.027*1e-24; 
-
-Omega=(Theta/(2*tau*sqrt(pi)))*exp(-(t./(2*tau))^2);    %Rabi frequency laser in 1/s
-K=coth(hbar*Omega/(2*kb*T))*A*((Omega)^3)*exp(-(Omega/wc)^2)*pi/2;   %Spectral density combined with temperature dependent term                 
+K = interp1(Kt,K,t); % Interpolate the data set (Kt,K) at time t
+Omega = interp1(Ot,Omega,t); % Interpolate the data set (Ot,Omega) at time t
 
 %% Master equation
-
-drho = zeros (3,1);        
-drho=[0 0 -Omega;0 -K 0; Omega 0 -K]*rho+[0;0;-Omega/2];     %Systems of differential equations.
+drho = zeros (4,1);        
+drho=[0 0 -Omega 0;0 -K 0 0; Omega 0 -K 0; 0 0 0 0]*rho+[0;0;-Omega/2;-rho(1)];     %Systems of differential equations.
 end
