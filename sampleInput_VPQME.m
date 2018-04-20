61d4ef02a25b252d5ed105e741f161956a619aaf
Camilo Chaparro <33160873+Camilokastel160@users.noreply.github.com>
2018-04-20 15:28:45 -0500

    Update sampleInput_VPQME.m with parameters
    Update with parameters, constants and functions editable directly without use the function VPQME.m

--- sampleInput_VPQME.m
+++ sampleInput_VPQME.m
@@ -1,19 +1,44 @@
-%% Preliminary version of VPQME.
+% Preliminary version of VPQME.
 
-totalT=4/1e11;          %Time of evolution in s     
+%% 1) Fundamental constants
+
+kb=1.38064852e-23;              %Boltzmann constant K/K
+hbar=1.0545718e-34;             %Planck constant Js
+
+%% 2) Paramaters of the system
+
+Theta=30;                     
+tau=14e-12;                     %Full width half maximum in s
+T=25;                           %Temperature in K
+wc=2.2e12;                      %1/s   
+alpha=pi*0.027*1e-24;           % prefactor. In s^2 / rad^2
+totalT=4/1e11;                  %Time of evolution in s   
+
+%% 3)Coefficients of the differential equation 
+
+Ot = linspace(0,5/1e11,100);
+Omega=(Theta/(2*tau*sqrt(pi)))*exp(-(Ot/(2*tau)).^2);
+
+Kt = linspace(0,4/1e11,100);
+K=3e11+Kt*0;            
+
+%% 4) Initial density matrix
+
 rho=[0.5 0.5; 0.5 0.5]; %Initial density matrix
+
+%% 5) ODE configuration
                                     
-options=odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-4]);
-[t,Y]=ode45(@VPQME,[0 totalT],[rho(1,1) real(rho(1,2)) imag(rho(1,2))],options); 
+options=odeset('RelTol',1e-4,'AbsTol',[1e-4 1e-4 1e-4 1e-4]);
+[t,Y]=ode45(@(t,rho) VPQME(t,rho,Kt,K,Ot,Omega,kb,hbar,Theta,tau,T,wc,alpha),[0 totalT],[rho(1,1) real(rho(1,2)) imag(rho(1,2)) rho(2,2)],options); 
 %[t,Y]=ode113(@VPQME,[0 totalT],[rho(1,1) real(rho(1,2)) imag(rho(1,2))],options);
 
-%% Plot of the RDM dynamics
+%% 6) Plot of the RDM dynamics
 
 figure(1);hold('on');
 plotHandle(1)=plot(t,Y(:,1),'k','LineWidth',5);
 plotHandle(2)=plot(t,Y(:,2),'r','LineWidth',5);
 plotHandle(3)=plot(t,Y(:,3),'b','LineWidth',5);
-plotHandle(4)=plot(t,1-Y(:,1),'color',[0.5 0.5 0.5],'LineWidth',5);
+plotHandle(4)=plot(t,Y(:,4),'color',[0.5 0.5 0.5],'LineWidth',5);
 
 legendHandle=legend(plotHandle,'$\langle1|\rho(t)|1\rangle$','$\Re\langle1|\rho(t)|0\rangle$','$\Im\langle1|\rho(t)|0\rangle$','$\langle0|\rho(t)|0\rangle$');
 set(legendHandle,'Interpreter','latex','FontSize',32,'LineWidth',2);
@@ -28,6 +53,8 @@ yticklabels('auto');
 
 set(gcf, 'Color', 'w');
 set(gcf,'renderer','Painters');
+
+%% 7) Plot of pxx.
 
 figure(2);hold('on');
 plotHandle(1)=plot(t,Y(:,1),'k','LineWidth',5);
